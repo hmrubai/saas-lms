d1333e11d96d97a3cb3aaef6ccc4809e5f01d210
arfin foysal <arfinfoysalbd@gmail.com>
2023-07-19 17:08:13 +0600

    comp

--- app/Models/ConsumerRequest.php
+++ app/Models/ConsumerRequest.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class ConsumerRequest extends Model
 {
     use HasFactory;
+    protected $table = 'consumer_requests';
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'address',
+        'occupation',
+        'organization_name',
+        'organization_address',
+        'nid_passport',
+        'trade_license',
+        'web_address',
+        'post_code',
+    ];
 }
