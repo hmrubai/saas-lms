dd5f70a796b5048ed3c107904ac4be9c8215f372
arfin foysal <arfinfoysalbd@gmail.com>
2023-07-19 11:37:45 +0600

    course list

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -1,10 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
-    //
+
+    public function courseListByCategory(Request $request)
+    {
+        $courseList = Course::where('category_id', $request->category_id)
+            ->select('id', 'name')->get();
+        return response()->json([
+            'status' => true,
+            'message' => 'List Successful',
+            'data' => $courseList
+        ], 200);
+    }
 }
