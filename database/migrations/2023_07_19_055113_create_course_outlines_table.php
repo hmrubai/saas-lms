4da7ab9dcb84105557aecd5296785d277333d71e
hmrubai <rubai.mobarak@gmail.com>
2023-07-19 13:57:41 +0600

    Course Outline added

---
+++ database/migrations/2023_07_19_055113_create_course_outlines_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCourseOutlinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_outlines', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('title_bn')->nullable();
+            $table->bigInteger('course_id');
+            $table->bigInteger('class_level_id');
+            $table->bigInteger('subject_id');
+            $table->bigInteger('chapter_id');
+            $table->bigInteger('chapter_script_id');
+            $table->bigInteger('chapter_video_id');
+            $table->bigInteger('chapter_quiz_id');
+            $table->boolean('is_free')->default(0);
+            $table->integer('sequence')->default(0);
+            $table->boolean('is_active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_outlines');
+    }
+}
