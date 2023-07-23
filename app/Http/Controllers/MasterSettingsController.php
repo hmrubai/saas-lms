4525d729c40a2f057f6a93a53f4d3f077069ce4a
hmrubai <rubai.mobarak@gmail.com>
2023-07-23 09:10:51 +0600

    Course list changed

--- app/Http/Controllers/MasterSettingsController.php
+++ app/Http/Controllers/MasterSettingsController.php
@@ -210,18 +210,22 @@ class MasterSettingsController extends Controller
             $item->courses = $courses;
 
             foreach ($courses as $course) {
-                //CourseOutline
-                //CourseClassRoutine
-                //CourseFeature
-                //CourseMentor
-                //CourseFaq
+                $course->course_outline = CourseOutline::select(
+                        'course_outlines.*', 
+                        'class_levels.name as class_name', 
+                        'subjects.name as subject_name',
+                        'chapters.name as chapter_name'
+                    )
+                    ->where('course_outlines.course_id', $course->id)
+                    ->leftJoin('class_levels', 'class_levels.id', 'course_outlines.class_level_id')
+                    ->leftJoin('subjects', 'subjects.id', 'course_outlines.subject_id')
+                    ->leftJoin('chapters', 'chapters.id', 'course_outlines.chapter_id')
+                    ->get();
 
-                $course->course_outline = CourseOutline::where('course_id', $course->id)->get();
                 $course->course_routine = CourseClassRoutine::where('course_id', $course->id)->get();
                 $course->course_feature = CourseFeature::where('course_id', $course->id)->get();
                 $course->course_mentor = CourseMentor::where('course_id', $course->id)->get();
-                $course->course_gaq = CourseFaq::where('course_id', $course->id)->get();
-
+                $course->course_faq = CourseFaq::where('course_id', $course->id)->get();
             }
         }
 
