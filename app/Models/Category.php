2cc37f064f29885ee327987cd08e600f4b6cee6e
hmrubai <rubai.mobarak@gmail.com>
2023-07-31 10:27:28 +0600

    Service Added

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -14,6 +14,7 @@ class Category extends Model
     protected $fillable = [
         'name',
         'link',
+        'sequence',
         'is_authentication_needed',
         'has_submenu',
         'is_course',
