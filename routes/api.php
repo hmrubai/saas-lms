a328303107ff37fa925b77d207d172471b54811c
hmrubai <rubai.mobarak@gmail.com>
2023-08-28 10:37:43 +0600

    Purchase list completed

--- routes/api.php
+++ routes/api.php
@@ -86,6 +86,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
         Route::get('student-course-list', [StudentController::class, 'myCourseList']);
         Route::get('student-class-list', [CourseController::class, 'studentClassList']);
+        Route::get('student-purchase-list', [StudentController::class, 'myPurchaseList']);
     });
 
 //All Admin Routes Start
